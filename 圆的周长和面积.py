4a3b64100510db18edd3406a9fba67c30982dbe6
limeng-gi <92004605+limeng-gi@users.noreply.github.com>
2021-10-09 14:44:59 +0800

    Add files via upload

---
+++ 圆的周长和面积.py
@@ -0,0 +1,67 @@
+# r=int(input("请输入一个整数"))
+# print(r)
+# Π=3.14
+# r=int(r)
+# zc=(2*Π*r)
+# mj=(Π*r*r)
+# print(zc,mj)
+
+
+
+
+
+
+# 三角形面积
+#
+# a = float(input('输入三角形第一边长: '))
+# b = float(input('输入三角形第二边长: '))
+# c = float(input('输入三角形第三边长: '))
+#
+# # 计算半周长
+# s = (a + b + c) / 2
+#
+# # 计算面积
+# area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+# print('三角形面积为:%.2f' %area)
+
+
+
+
+# import random as a
+# while True:
+#  num=input("请输入成绩为:")
+#  if num.isdigit():
+#      num=int(num)
+#      if num >= 90 and num <=100:
+#          print("A")
+#      elif num >=80 and num <90:
+#          print("B")
+#      elif num >=70 and num <80:
+#          print("C")
+#      elif num >=60 and num <70:
+#          print("D")
+#      elif num >100:
+#          print("成绩错误")
+#
+#      else:
+#          print("E")
+
+# num=float(input("请输入一个温度："))
+# print(num)
+# num1=((num-32)/1.8)
+# print(round(num1,1))
+
+
+# year = int(input("输入一个年份: "))
+# if (year % 4) == 0:
+#     if(year % 100) ==0:
+#        if (year % 400) == 0:
+#            print("{0} 是闰年".format(year))   # 整百年能被400整除的是闰年
+#        else:
+#            print("{0} 不是闰年".format(year))
+#     else:
+#         print("{0}是闰年".format(year))
+# else:
+#    print("{0} 不是闰年".format(year))
+
+
